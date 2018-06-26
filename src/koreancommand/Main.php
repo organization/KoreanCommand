10513ca674e8bfe69e7bf4f7805dd832bf2a06db
maru <wsj7178@gmail.com>
2018-06-27 07:50:53 +0900

    update pluginManager

--- src/koreancommand/Main.php
+++ src/koreancommand/Main.php
@@ -7,12 +7,16 @@ use koreancommand\command\KoreanCommandMap;
 
 class Main extends PluginBase {
 	public function onLoad() {
+		$this->initCommandMap();
+		//$this->initCommandReader();
+	}
+	
+	private function initCommandMap() {
 		$server = $this->getServer();
 		
 		$serverCls = new \ReflectionClass($server);
 		$commandMapProperty = $serverCls->getProperty("commandMap");
 		$commandMapProperty->setAccessible(true);
-		
 		
 		$currentCommandMap = $commandMapProperty->getValue($server);
 		
@@ -21,9 +25,24 @@ class Main extends PluginBase {
 		$mapCls = new \ReflectionClass($currentCommandMap);
 		$commandsProperty = $mapCls->getProperty("knownCommands");
 		$commandsProperty->setAccessible(true);
-		$commandsProperty->setValue($krMap, $commandsProperty->getValue($currentCommandMap));
+		$commandsProperty->setValue($krMap, $currentCommandMap->getCommands());
+		
+		$manager = $this->getServer()->getPluginManager();
+		$managerCls = new \ReflectionClass($manager);
+		$managerMapProperty = $managerCls->getProperty("commandMap");
+		$managerMapProperty->setAccessible(true);
 		
 		$commandMapProperty->setValue($server, $krMap);
+		$managerMapProperty->setValue($manager, $krMap);
+	}
+	
+	private function initCommandReader() {
+		$server = $this->getServer();
+		
+		$serverCls = new \ReflectionClass($server);
+		$readerCls = $serverCls->getProperty("console");
+		
+		
 	}
 }
 
